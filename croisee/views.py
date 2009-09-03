0d5e198e817f10df687578b04768a6e937b4bfc3
Henning Hraban Ramm <hraban@fiee.net>
2009-09-03 16:22:34 +0200

    enabled selecting dictionaries for the search

--- croisee/views.py
+++ croisee/views.py
@@ -6,25 +6,33 @@ from croisee.models import Dictionary, Word, cleanword
 
 def index(request, *args, **kwargs):
     dictionaries = Dictionary.objects.all()
+    forbidden_dicts = []
+    context = {
+        'MEDIA_URL':    settings.MEDIA_URL,
+        'dictionaries': dictionaries,
+        'posted':       False,
+        'results':      [],
+        'resultcount':  0,
+        'searchterm':   '',
+    }
+    if request.POST:
+        context['posted'] = True
+        for d in dictionaries:
+            if 'dic_%d' % d.id not in request.POST:
+                d.disabled = True
+                forbidden_dicts.append(d.id)
     try:
         searchterm = cleanword(request.POST['searchterm'], False).upper()
-        posted = True
         # REGEX search
         #internal = searchterm.replace('?','.').replace('_','.').replace('*','.+').replace('%','.+')
         #results = Word.objects.filter(word__regex=r'^%s$' % internal)
         # EXTRA search
         internal = searchterm.replace('?','_').replace('*','%').replace('%','%%')
-        results = Word.objects.extra(where=['word LIKE "' + internal + '"'])
+        results = Word.objects.extra(where=['word LIKE "'+internal+'"']).exclude(dictionary__id__in=forbidden_dicts)
         # TODO: dictionaries in selected
-    except (KeyError, Word.DoesNotExist):
-        searchterm = ''
-        results = None
-        posted= False
-    return render_to_response('index.html', {
-        'MEDIA_URL':    settings.MEDIA_URL,
-        'dictionaries': dictionaries,
-        'searchterm':   searchterm,
-        'results':      results,
-        'posted':       posted,
-        'resultcount':  len(results),
-    })
+        context['resultcount'] = len(results)
+        context['results'] = results
+        context['searchterm'] = searchterm
+    except (KeyError, Word.DoesNotExist), ex:
+        pass
+    return render_to_response('index.html', context)
