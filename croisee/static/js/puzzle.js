c66cd2d56d27f1199cfd69149bfb9c585c6179ed
Henning Hraban Ramm <hraban@fiee.net>
2009-09-09 13:29:33 +0200

    key handling changed, numbering of fields added

--- croisee/static/js/puzzle.js
+++ croisee/static/js/puzzle.js
@@ -1,5 +1,6 @@
 var letters = 'ABCDEFGHIJKLMNOPQRTSUVWXYZ ';
-var goodkeys = [9,32,37,38,39,40,27];
+var goodkeys = [9,32,37,38,39,40,46,51,191];
+var modifiers = [16,17,18,91];
 
 function getWord(x,y,maxx,maxy) {
 	/*
@@ -49,28 +50,62 @@ function getWord(x,y,maxx,maxy) {
 	}
 	var query = '/ajax/'+horiz+','+xpos+'/'+vert+','+ypos+'/';
 	return $.get(query, {}, function(data){
-		console.log(data);
 		$('#result').html(data);
 	}, 'html');
-	// TODO: make result clickable and insert on click, then requery
+	// TODO: make result clickable and insert on click
+}
+
+function renumberPuzzle(x,y,maxx,maxy,reset) {
+	var id = '#num_'+y+'_'+x;
+	if (reset||$(id).html()) {
+		$(id).html('');
+	} else {
+		$(id).html('#');
+	}
+	var num=1;
+	for (cy=1; cy<=maxy; cy++) {
+		for (cx=1; cx<=maxx; cx++) {
+			var nf = $('#num_'+cy+'_'+cx);
+			if (nf.html()) {
+				nf.html(num++);
+			}
+		}
+	}
 }
 
 $(function(){
 	var maxx = Number($('input#maxx').val());
 	var maxy = Number($('input#maxy').val());
+	$('input.puzzlechar').keypress(function(event){
+		/* handle character keys A-Z */
+		if ( modifiers.indexOf(event.keyCode) < 1 ) { // no modifiers except shift
+			event.preventDefault();
+			if ((65 <= event.which && event.which <= 65 + 25) 
+				|| (97 <= event.which && event.which <= 97 + 25)) {
+		        var c = String.fromCharCode(event.which).toUpperCase();
+				$(this).val(c);
+				//console.log(event.keyCode, c);
+			}
+			return false;
+		}
+	});
 	$('input.puzzlechar').keyup(function(event){
-		//console.log(event.keyCode);
-		event.preventDefault();
-		$(this).val(this.value.toUpperCase());
+		/* handle any other keys */
+		//console.log(event.keyCode, 'UP');
 		var idp = this.id.split('_'); // char,y,x
 		var x = idp[2];
 		var y = idp[1];
 		switch (event.keyCode) {
+			case 8: // backspace
+				$(this).val('');
+				x--;
+				break;
 			case 9: // tab
 				//if (event.shiftKey) { x--; } else { x++; }
 				break;
 			case 32: // space
 				$(this).val('.').parent('td').toggleClass('blocked');
+				renumberPuzzle(x,y,maxx,maxy,true);
 				x++;
 				break;
 			case 37: // left
@@ -85,34 +120,38 @@ $(function(){
 			case 40: // down
 				y++;
 				break;
-			case 27: // escape
-				// TODO: change to other key
-				console.log(getWord(x,y,maxx,maxy));
+			case 46: // forward delete
+				$(this).val('');
+				x++;
+				break;
+			case 51: // number sign #
+				renumberPuzzle(x,y,maxx,maxy,false);
+				break;
+			case 191: // escape
+				getWord(x,y,maxx,maxy);
 				break;
 			default:
 				$(this).parent('td').removeClass('blocked');
-				x++;
+				if (event.shiftKey) { y++; } else { x++; }
 		}
 		if (x>maxx) {
 			x=1;
-			//y++;
 		} else if (x<1) {
 			x=maxx;
-			//y--;
 		}
 		if (y>maxy) {
 			y=1;
 		} else if (y<1) {
 			y=maxy;
 		}
-		if ( ( (event.keyCode > 64) && (event.keyCode < 91) ) || ( goodkeys.indexOf(event.keyCode) !== -1 ) ) {
+		event.preventDefault();
+		if ( modifiers.indexOf(event.keyCode) == -1 ) {
 			$('#char_'+y+'_'+x).focus().select();
-		} else {
-			$(this).val('');
 		}
 		return true;
 	});
 	$('input.numeric').keyup(function(event){
+		/* handle numeric input fields */
 		event.preventDefault();
 		var num = Number(this.value);
 		if (isNaN(num)) num=0;
