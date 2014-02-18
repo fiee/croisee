ebf6dc329da13a33c35e9798adfa87425cd61a69
Henning Hraban Ramm <hraban@fiee.net>
2014-02-19 02:22:41 +0600

    Trying to find a working setup with gunicorn and supervisor

--- gunicorn-settings.py
+++ gunicorn-settings.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-from croisee import settings
+#from croisee import settings
+PROJECT_NAME='croisee'
+rootrel = lambda p: os.path.normpath(os.path.join('/var/www', PROJECT_NAME, p))
 
 bind = '127.0.0.1:8'+str(os.getuid())[1:]
 workers = 2
 #worker_class = 'eventlet'
 #max_requests = 2048
-pidfile = settings.rootrel('logs/django.pid')
-user = settings.PROJECT_NAME
-group = settings.PROJECT_NAME
-logfile = settings.rootrel('logs/gunicorn.log')
+pidfile = rootrel('logs/django.pid')
+user = PROJECT_NAME
+group = PROJECT_NAME
+logfile = rootrel('logs/gunicorn.log')
 #loglevel = 'info'
-proc_name = 'gunicorn-'+settings.PROJECT_NAME
+proc_name = 'gunicorn-'+PROJECT_NAME
